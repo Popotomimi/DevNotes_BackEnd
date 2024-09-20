389fbfa87b4c51cd9a7c96cae0ec86cdceb1b0fc
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-09-20 19:13:47 -0300

    Remove cros
    Remove cros

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT ? Number(process.env.PORT) : 3333;
+const port = process.env.PORT || 3333;
 
 const app = express();
 
